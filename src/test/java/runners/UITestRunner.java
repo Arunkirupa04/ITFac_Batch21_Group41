1c22f287101089cb48fea57b38cc7ec4e67866ab
Aranya2402 <aranya24arii@gmail.com>
2026-01-29 18:36:58 +0530

    Added admin login code

--- src/test/java/runners/UITestRunner.java
+++ src/test/java/runners/UITestRunner.java
@@ -1,4 +1,16 @@
 package runners;
 
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "stepdefinitions.ui",
+        plugin = {"pretty", "html:target/cucumber-report.html"},
+        monochrome = true,
+        tags = "@admin_login"  // <-- run only scenarios with this tag
+)
 public class UITestRunner {
 }
